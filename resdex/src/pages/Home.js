77ebdf22de283ed92e53a0b9bb4480ac1e726ac6
Dev Patel <devptl841806@gmail.com>
2024-10-13 23:48:58 -0400

    baseline

--- resdex/src/pages/Home.js
+++ resdex/src/pages/Home.js
@@ -1,11 +1,22 @@
 // src/pages/Home.js
 import React from 'react';
+import Headline from '../images/headline.png';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of your application.</p>
+    <div className='background'>
+        <div className="center-container title">
+           <div className='row length'>
+                <div className='col-md-5 col-center border-view'>
+                    Research Made Easy.
+                </div>
+                <div className='col-md-7 col-center border-view'>
+                <img id="headline-img" className="img-fluid" src={Headline} alt="ResDex Logo" />
+
+                </div>
+           </div>
+        </div>
+       
     </div>
   );
 };
