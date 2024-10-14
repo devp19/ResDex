c6db9c1aa75b8f0e356e14f2fd6ad434b1f5be10
Dev Patel <devptl841806@gmail.com>
2024-10-14 00:00:52 -0400

    main landing

--- resdex/src/pages/Home.js
+++ resdex/src/pages/Home.js
@@ -7,10 +7,27 @@ const Home = () => {
     <div className='background'>
         <div className="center-container title">
            <div className='row length'>
-                <div className='col-md-5 col-center border-view'>
+                <div className='col-md-5 offset-md-1 col-center'>
+            
+                    <div className='row'>
                     Research Made Easy.
+                    <br></br>
+                    <span className='regular-text'>
+                       ↓
+                       <br></br>
+
+                       For students, by students.
+                       <br></br>
+                       <span className='smaller-text'>
+                        Share research, gain insightful feedback and connect with industry professionals in over 100+ topics.
+                       </span>
+                    </span>
+                    
+                    </div>
+                   
                 </div>
-                <div className='col-md-7 col-center border-view'>
+                
+                <div className='col-md-5 col-center'>
                 <img id="headline-img" className="img-fluid" src={Headline} alt="ResDex Logo" />
 
                 </div>
