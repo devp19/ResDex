745048ddb0fc395e5d25c2e024ed87b5b8a05bbe
Dev Patel <devptl841806@gmail.com>
2024-10-14 20:00:57 -0400

    about + objective mainframe

--- resdex/src/pages/About.js
+++ resdex/src/pages/About.js
@@ -4,9 +4,36 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <h1>Welcome to the About Page</h1>
-      <p>This is the About Page!</p>
+      <h1 className='pt-4'>About → ResDex</h1>
+      <div className='row'>
+      <p className='col-md'>
+        ResDex is a student-led organization aimed towards bridging the gap between academia and industry-level research publications, making it easier for students to find research-assistant positions and go beyond the traditional academic scope to create impactful research papers.
+
+      <br></br>
+      <br></br>
+Giving students the opportunity to stand out among millions, review and edit papers, and connect with PHD level researchers, ResDex is designed to foster a collaborative academic community, allowing users to showcase their research, achievements, and certifications in a dynamic online portfolio.
+
+By engaging in peer and expert reviews, students can refine their work, gain valuable feedback, and build meaningful connections with academics and professionals in their field.
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+      <h3> Objective</h3>
+      We understand the difficulty and often timely process of cold-emailing hundreds of professors for research positions. While all is said and done, research itself is a hard industry to really gain experience in.
+    <br></br>
+    <br></br>
+“That’s where ResDex comes into play.”
+<br></br>
+    <br></br>
+As a research-sharing platform, designed for students by students, we tailored the platform to fit those exact needs. Everything a student needs all on one site to help the user really build a portfolio around their contributions to the world of research.
+
+      </p>
+      </div>
+      <br></br>
+      <br></br>
+    
     </div>
+
   );
 };
 
