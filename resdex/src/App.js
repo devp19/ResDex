503d15401e8b301029502c15f7da66c581b84b1d
Dev Patel <devptl841806@gmail.com>
2025-01-20 22:43:38 -0500

    development mode
    --> limited access for development, sign in using access code.
    
    --> will turn off soon, navbar icon reverted to text indefinitely.

--- resdex/src/App.js
+++ resdex/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -28,32 +28,65 @@ function ScrollToTop() {
 }
 
 function App() {
+  const [accessCode, setAccessCode] = useState('');
+  const [hasAccess, setHasAccess] = useState(false);
+
+  const handleAccessCodeSubmit = (e) => {
+    e.preventDefault();
+    if (accessCode === 'devp19') {
+      setHasAccess(true);
+    } else {
+      alert('Incorrect access code');
+    }
+  };
+
+  if (!hasAccess) {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '20%' }}>
+        <h1 className='primary monarque'>You're Early!</h1>
+        <p className='primary'>ResDex is currently in development mode. <br></br>Please check out our LinkedIn for more updates!</p>
+
+        <form onSubmit={handleAccessCodeSubmit}>
+          <input
+          className='primary'
+          style={{borderRadius: '5px', padding: '5px', marginTop: '20px', border: '1px solid black'}}
+            type="text"
+            value={accessCode}
+            onChange={(e) => setAccessCode(e.target.value)}
+            placeholder="Early Access Code"
+          />
+          <br></br>
+<button type='submit' className="custom" style={{marginBottom: '20px' }}>
+                Enter
+              </button>        </form>
+      </div>
+    );
+  }
 
   return (
     <Router>
       <ScrollToTop />
       <div className="App">
-       
-          <>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/recovery" element={<Recovery />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/success" element={<Success />} />
-              <Route path="/create" element={<Create />} />
-              <Route path="/notifications" element={<Notifications />} />
-              <Route path="/releasedocs" element={<ReleaseDocs />} />
-              <Route path="/releasedocs/v101" element={<V101 />} />
-              <Route path="/profile/:username" element={<Profile />} />
-            </Routes>
-            <Footer />
-          </>
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/recovery" element={<Recovery />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/releasedocs" element={<ReleaseDocs />} />
+            <Route path="/releasedocs/v101" element={<V101 />} />
+            <Route path="/profile/:username" element={<Profile />} />
+          </Routes>
+          <Footer />
+        </>
       </div>
     </Router>
   );
