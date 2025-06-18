d415c13a136c77ebdd124f18b5befda1b5deeb90
Dev Patel <devptl841806@gmail.com>
2025-06-17 23:08:04 -0400

    ui update for success page
    neglect in releases

--- resdex/src/pages/Success.js
+++ resdex/src/pages/Success.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Success = () => {
 
     return(
         <div>
-                <h1 className='title center top'> Success! </h1>
+                <h1 className='title center primary monarque top fade-in2'> Success! </h1>
                 
-                <p className='center'>Ticket submitted successfully!</p>
+                <p className='center primary fade-in2'>Ticket submitted successfully!</p>
+
+                <div className='smaller-text center button-custom fade-in2'>
+                              <Link className='custom' to='/'>Back Home</Link>
+                            </div>
         </div>
     );
 
