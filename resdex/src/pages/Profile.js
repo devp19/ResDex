5f890d1bbe1ede99f573a58d8f423ed0d2dd81c2
Dev Patel <devptl841806@gmail.com>
2025-06-21 17:04:32 -0400

    Fix SVG className usage and minor imports in pages
    Replaces incorrect 'class' attributes with 'className' in SVG elements in Profile.js to ensure proper React rendering. Also adds missing CSS import in ChatBox.js and imports useNavigate in Search.js for future navigation handling.

--- resdex/src/pages/Profile.js
+++ resdex/src/pages/Profile.js
@@ -1224,63 +1224,36 @@ const Profile = () => {
                 className="col-md d-flex justify-content-end mt-4"
                 style={{ maxHeight: "50px" }}
               >
-                {!isOwnProfile && (
+                { !isOwnProfile && (
+                  <>
                     <a
                       className="custom-view"
                       onClick={toggleFollow}
                       disabled={isRequestInProgress || requestSent}
-                      style={{ marginRight: "10px" }}
+                      style={{ marginRight: '10px' }}
                     >
                       {requestSent ? (
-                        <span style={{ position: "relative", top: "7px" }}>
-                          Request Sent
-                          <svg
-                            style={{ marginLeft: "10px" }}
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="white"
-                            class="bi bi-send-plus-fill"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 1.59 2.498C8 14 8 13 8 12.5a4.5 4.5 0 0 1 5.026-4.47zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
-                            <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5" />
+                        <span style={{ position: 'relative', top: '7px' }}>Request Sent
+                          <svg style={{ marginLeft: '10px' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-send-plus-fill" viewBox="0 0 16 16">
+                            <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 1.59 2.498C8 14 8 13 8 12.5a4.5 4.5 0 0 1 5.026-4.47zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
+                            <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5"/>
                           </svg>
                         </span>
                       ) : isFollowing ? (
-                        <span style={{ position: "relative", top: "7px" }}>
-                          Unfollow
-                          <svg
-                            style={{ marginLeft: "10px" }}
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="white"
-                            class="bi bi-people-fill"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
+                        <span style={{ position: 'relative', top: '7px' }}>Unfollow
+                          <svg style={{ marginLeft: '10px' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-people-fill" viewBox="0 0 16 16">
+                            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
                           </svg>
                         </span>
                       ) : (
-                        <span style={{ position: "relative", top: "7px" }}>
-                          Follow
-                          <svg
-                            style={{ marginLeft: "10px" }}
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="white"
-                            class="bi bi-person-fill-add"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3.5 3.5 0 0 1 6 0" />
-                            <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
+                        <span style={{ position: 'relative', top: '7px' }}>Follow
+                          <svg style={{ marginLeft: '10px' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-person-fill-add" viewBox="0 0 16 16">
+                            <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3.5 3.5 0 0 1 6 0"/>
+                            <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
                           </svg>
                         </span>
                       )}
                     </a>
-
                     <a
                       className="custom-view chat-button"
                       onClick={handleChatClick}
@@ -1421,7 +1394,7 @@ const Profile = () => {
                         width="16"
                         height="16"
                         fill="primary"
-                        class="bi bi-person-lines-fill"
+                        className="bi bi-person-lines-fill"
                         viewBox="0 0 16 16"
                       >
                         <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
