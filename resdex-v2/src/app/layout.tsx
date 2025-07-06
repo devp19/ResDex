0930b871b3078aab7bf88527d5570d3c7af7d47d
Dev Patel <devptl841806@gmail.com>
2025-07-06 19:24:05 -0400

    added favicon icon

--- resdex-v2/src/app/layout.tsx
+++ resdex-v2/src/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/beige-logo.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
