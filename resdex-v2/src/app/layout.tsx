f5813f8b740207e6bfb08a427ddb29f11f834786
Dev Patel <devptl841806@gmail.com>
2025-08-30 23:09:07 -0400

    analytics vercel update

--- resdex-v2/src/app/layout.tsx
+++ resdex-v2/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { SmoothCursor } from "@/components/ui/SmoothCursor";
 import { AuthProvider } from '@/components/auth-provider';
 import NotificationSidebarProvider from '@/components/ui/NotificationSidebarProvider';
 import { Footer7 } from "@/components/footer7";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,8 @@ export default function RootLayout({
             <div className="flex flex-col min-h-screen">
               <main className="flex-1">
                 {children}
+                <Analytics />
+
               </main>
               <div className="flex justify-center w-full">
                 <Footer7
